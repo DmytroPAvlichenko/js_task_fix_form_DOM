fb59e75d3deffaa64ba6166879b64e0f49ab8dd8
dimon <80713na@gmail.com>
2025-03-26 23:53:16 +0200

    class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ inputs.forEach((input) => {
   const label = document.createElement('label');
 
   label.setAttribute('for', input.id);
+  label.classList.add('field-label');
   label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
   input.setAttribute(
